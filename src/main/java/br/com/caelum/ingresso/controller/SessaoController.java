f74a1a9a606e43e6d9d381b9b256d01c8d388058
Endrew <endmuller@gmail.com>
2018-01-13 14:40:28 -0200

    correcao

--- src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
+++ src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
@@ -21,45 +21,45 @@ import br.com.caelum.ingresso.validacao.GerenciadorDeSessao;
 
 @Controller
 public class SessaoController {
-	
+
 	@Autowired
 	private SalaDao salaDao;
 	@Autowired
 	private FilmeDao filmeDao;
 	@Autowired
 	private SessaoDao sessaoDao;
-	
+
 	@GetMapping("/admin/sessao")
 	public ModelAndView form(@RequestParam("salaId") Integer salaId, SessaoForm form) {
-		
+
 		form.setSalaId(salaId);
-		
+
 		ModelAndView modelAndView = new ModelAndView("sessao/sessao");
-		
+
 		modelAndView.addObject("sala", salaDao.findOne(salaId));
 		modelAndView.addObject("filmes", filmeDao.findAll());
 		modelAndView.addObject("form", form);
-		
+
 		return modelAndView;
 	}
-	
+
 	@PostMapping(value = "/admin/sessao")
 	@Transactional
-	public ModelAndView salva(@Valid SessaoForm form, BindingResult result){
-		
-		if (result.hasErrors()) return form(form.getSalaId(),form);
-		
-		ModelAndView modelAndView = new ModelAndView("redirect:/admin/sala/"+form.getSalaId()+"/sessoes");
-		
+	public ModelAndView salva(@Valid SessaoForm form, BindingResult result) {
+
+		if (result.hasErrors())
+			return form(form.getSalaId(), form);
+
+		ModelAndView modelAndView = new ModelAndView("redirect:/admin/sala/" + form.getSalaId() + "/sessoes");
+
 		Sessao sessao = form.toSessao(salaDao, filmeDao);
-		
-		sessaoDao.save(sessao);
+
 		Sala sala = salaDao.findOne(form.getSalaId());
 		GerenciadorDeSessao gerenciadorDeSessao = new GerenciadorDeSessao(sessaoDao.buscaSessoesDaSala(sala));
-				if(gerenciadorDeSessao.cabe(sessao)){
-					sessaoDao.save(sessao);
-				}
-		
+		if (gerenciadorDeSessao.cabe(sessao)) {
+			sessaoDao.save(sessao);
+		}
+
 		return modelAndView;
 	}
 
